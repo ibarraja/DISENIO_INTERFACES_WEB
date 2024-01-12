6f8205c03811d948d7e32bce3791bddfb57d586c
ibarraja <javier.ibarra.mz@gmail.com>
2024-01-12 10:34:56 +0100

    Tailwind Grid v2

---
+++ 2do Trimestre/UT6 Framework tailwind/2024-01-12a Maquetacion con CLI/tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    
+    colors: {
+      'my-blue': '#1fb6ff',
+      'my-orange': '#ff7849',
+      'my-green': '#13ce66',
+      'my-yellow': '#ffc82c',
+      'my-gray': '#8492a6',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
